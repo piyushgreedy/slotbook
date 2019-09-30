89a5524e66ec81222ed30a1ee133e3d8fa7db95f
Piyush <piyush@viewlift.com>
2019-09-30 15:28:40 +0530

    slotly

---
+++ Slotly/Slotly/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Slotly.Models;
+using RequestHandlers;
+
+namespace Slotly.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            var userName = Request.Query["id"];
+            Console.WriteLine("piyushindex");
+            ViewData["userName"] = userName.ToString().ToUpper();
+            if (String.IsNullOrEmpty(ViewData["userName"].ToString())){
+                ViewData["userName"] = "Guest User. Please Login to Book Computer Time Slot";
+            }
+            return View();
+           
+        }
+
+        public IActionResult About()
+        {   
+            Console.WriteLine("piyushabout");
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ReceiveWithRequestFormData(string username, string password)
+        {
+            System.Diagnostics.Debug.WriteLine("Request Form Data");
+            System.Diagnostics.Debug.WriteLine("username");
+            //return Content("Login successful !"+ username);
+            LoginContext loginContext = HttpContext.RequestServices.GetService(typeof(LoginContext)) as LoginContext;
+            Boolean isAuthorised = loginContext.verifyLoginValues(username,password);
+            if (isAuthorised){
+                string url = string.Format("/Home/Index?id={0}", username);
+                return Redirect(url);
+            }
+            else {
+                return Content("UnAuthorised");
+            }   
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Slotly/Slotly/Models/LoginContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Slotly.Models
+{
+    public class LoginContext
+    {
+        public string ConnectionString { get; set; }
+
+        public LoginContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public Boolean verifyLoginValues(string username, string password)
+        {
+            Console.WriteLine("Piyush Test Connection");
+            Console.WriteLine(this.ConnectionString);
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from login_slot_book", conn);
+                Console.WriteLine(cmd);
+                using (MySqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Console.WriteLine("Piyush connected Connection");
+                        Console.WriteLine(dr.GetString("username"));
+                        Console.WriteLine(dr.GetString("password"));
+                        if(username== dr.GetString("username") && password== dr.GetString("password"))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public Boolean insertSlotDetail(string username, int startTime, int endTime, int dateTimeStamp)
+        {
+            return true;
+        }
+
+        public void getLoginDetails(string username)
+        {
+            Console.WriteLine("Piyush Test Connection");
+            Console.WriteLine(this.ConnectionString);
+            
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from login_slot_book", conn);
+                Console.WriteLine(cmd);
+         
+                using (MySqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Console.WriteLine("Piyush connected Connection");
+                        Console.WriteLine(dr.GetString("username"));
+                        Console.WriteLine(dr.GetString("password"));
+                    }
+                }
+            }
+        }   
+    }
+}
