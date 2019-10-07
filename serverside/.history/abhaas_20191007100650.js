5e19e087252ca7d28e5cd6f30e24511195082454
Piyush <piyush@viewlift.com>
2019-10-07 10:09:32 +0530

    coection

---
+++ serverside/.history/abhaas_20191007100650.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'password',
+  database: 'sys'
+});
+connection.connect((err) => {
+  if (err){
+      console.log(err);
+      throw err;
+  }
+  console.log('Connected!');
+});
